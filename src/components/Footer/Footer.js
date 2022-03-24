7caa916bc5db77635dbd51baf9965d79939a8c55
Daniel Ugarov <tyradire@yandex.ru>
2022-03-24 18:10:23 +0300

    folder edit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__wrapper">
+        <p className="footer__copyright">&copy; 2021</p>
+        <ul className="footer__links">
+          <li><a href="https://practicum.yandex.ru/profile/web/" target="_blank" rel="noopener noreferrer" className="footer__link">Яндекс.Практикум</a></li>
+          <li><a href="https://github.com/tyradire/" target="_blank" rel="noopener noreferrer" className="footer__link">Github</a></li>
+          <li><a href="vk.com" target="_blank" rel="noopener noreferrer" className="footer__link">Vkontakte</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,15 +7,15 @@ function Portfolio() {
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <div className="portfolio__item">
-          <li><a href="vk.com" className="portfolio__subtitle">Статичный сайт</a></li>
+          <li><a href="https://github.com/tyradire/how-to-learn" target="_blank" rel="noopener noreferrer" className="portfolio__subtitle">Статичный сайт</a></li>
           <img src={arrowLink} alt="Ссылка" className="portfolio__link"/>
         </div>
         <div className="portfolio__item">
-          <li><a href="vk.com" className="portfolio__subtitle">Адаптивный сайт</a></li>
+          <li><a href="https://github.com/tyradire/russian-travel" target="_blank" rel="noopener noreferrer" className="portfolio__subtitle">Адаптивный сайт</a></li>
           <img src={arrowLink} alt="Ссылка" className="portfolio__link"/>
         </div>
         <div className="portfolio__item">
-          <li><a href="vk.com" className="portfolio__subtitle">Одностраничное приложение</a></li>
+          <li><a href="https://github.com/tyradire/mesto" target="_blank" rel="noopener noreferrer" className="portfolio__subtitle">Одностраничное приложение</a></li>
           <img src={arrowLink} alt="Ссылка" className="portfolio__link"/>
         </div>
       </ul>
